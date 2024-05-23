85b8d4561e7fc6170d40768ef4b93aa3a759930d
hoga30 <tecyrose30@gmail.com>
2024-05-23 15:24:17 +0300

    booking info

---
+++ route/booking.router.js
@@ -0,0 +1,13 @@
+import express from "express";
+const bookingRoute = express.Router();
+import { addNewBooking, getAllBookings, getBookingById,updateBooking, deleteBooking, findBookingByCategory } from "../controller/booking.controller.js";
+import { addNewBookingValidation } from "../utils/validation.js";
+
+bookingRoute.post("/add", addNewBookingValidation,addNewBooking );
+bookingRoute.get("/list", getAllBookings);
+bookingRoute.get("/get/:id", getBookingById);
+bookingRoute.get("/getByCategory/:category", findBookingByCategory);
+bookingRoute.put("/update/:id", updateBooking);
+bookingRoute.delete("/delete/:id", deleteBooking);
+
+export default bookingRoute;
